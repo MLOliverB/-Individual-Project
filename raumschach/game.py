a0a46f4a3b19cb8d16954fa386c258abc053d5c7
Oliver Billich <2469811b@student.gla.ac.uk>
2022-10-18 22:24:23 +0100

    #3 #5 #6 - Implemented more game rules #3 Implemented game to stop at a draw with threefold repetition #5 Implemented game to stap at a draw if no progress in last 50 moves closes #3, closes #5

--- raumschach/game.py
+++ raumschach/game.py
@@ -1,6 +1,6 @@
 
 from raumschach.board import INITIAL_5_5_BOARD_SETUP, BoardState, ChessBoard
-from raumschach.figures import FIGURE_ID_MAP, Colour
+from raumschach.figures import FIGURE_ID_MAP, Colour, Pawn
 
 class IllegalActionException(Exception):
     """ illegal action exception class """
@@ -28,6 +28,16 @@ class ChessGame():
         while not any(self.is_checkmate):
             self.turn(player_num)
             player_num = (player_num + 1) % len(self.players)
+        if all(self.is_checkmate):
+            print("Draw!")
+        else:
+            for i in range(len(self.is_checkmate)):
+                if self.players[i][1] == Colour.WHITE:
+                    print("White wins!")
+                    break
+                else:
+                    print("Black wins!")
+                    break
 
     def turn(self, player_num):
         player, colour = self.players[player_num]
@@ -49,24 +59,52 @@ class ChessGame():
         elif (from_pos in moves and to_pos not in moves[from_pos]) and (from_pos in captures and to_pos not in captures[from_pos]):
             raise IllegalActionException("The given action is not part of the currently legal moves or captures.")
 
+        # Update the no progress rule
+        self.no_progress += 1
+        if FIGURE_ID_MAP[self.chess_board[from_pos]][0] == Pawn:
+            self.no_progress == 0
+        elif from_pos in captures and to_pos in captures[from_pos]:
+            self.no_progress == 0
+        elif self.no_progress > 50:
+            self.is_checkmate[player_num] = True
+            self.is_checkmate[(player_num + 1) % len(self.players)] = True
+            self.players[player_num][0].receive_reward(0, self.move_history[-1])
+            self.players[(player_num + 1) % len(self.players)][0].receive_reward(0, self.move_history[-1])
+            return "No progress has been achieved the last 50 turns (No pawn moved & no piece captured)." # Draw
+
         # Implement the action on the chess board
         from_figure_id = FIGURE_ID_MAP[self.chess_board[from_pos]] # Get the figure standing at the from position
         to_figure_id = FIGURE_ID_MAP[self.chess_board[to_pos]] if self.chess_board[to_pos] != 0 else None # Get the figure standing at the to position
         self.chess_board.move(from_pos, to_pos)
 
-        # Determine whether the game is over (either checkmate or stalemate)
+        # Update the board state repetition map
+        hash_val = self.chess_board.cube.data.tobytes()
+        if hash_val in self.state_repetition_map:
+            self.state_repetition_map[hash_val] += 1
+        else:
+            self.state_repetition_map[hash_val] = 1
+        # Check if threefold repetition rule applies
+        if self.state_repetition_map[hash_val] >= 3:
+            self.is_checkmate[player_num] = True
+            self.is_checkmate[(player_num + 1) % len(self.players)] = True
+            self.players[player_num][0].receive_reward(0, self.move_history[-1])
+            self.players[(player_num + 1) % len(self.players)][0].receive_reward(0, self.move_history[-1])
+            return "The same board position has been repeated for the third time." # Draw
+        
+        # If the same state was repeated three times, then this will result in an automatic draw
 
+        # Determine whether the game is over (either checkmate or stalemate)
 
         # Determine whether we have a check situation
 
-
         # Record the move in the move history
         self._record_move(action, from_figure_id, to_figure_id)
+        print(self.move_history)
 
         # Reward of moves generally is 0 - Reward of win is +1 - Reward of loss is -1 - Reward of draw is 0
         reward = 0
 
-        player.receive_reward(reward, self.move_history[-1])
+        player.receive_reward(0, self.move_history[-1])
 
 
     def _record_move(self, action, from_figure_id, to_figure_id):
