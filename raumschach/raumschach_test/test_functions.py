26b4adc523ca17480c53b7b11cab9f3f15855a61
Oliver Billich <2469811b@student.gla.ac.uk>
2023-03-22 19:02:11 +0000

    Fixed bug

--- raumschach/raumschach_test/test_functions.py
+++ raumschach/raumschach_test/test_functions.py
@@ -6,7 +6,8 @@ from raumschach.raumschach_learn.learn import network_setup
 
 def load_model(disk_path):
     rng, device, model, optimizer, memory = network_setup(5)
-    model.load_state_dict(torch.load(disk_path, weights_only=True, map_location=torch.device('cpu')).to(device))
+    model.load_state_dict(torch.load(disk_path, weights_only=True, map_location=torch.device('cpu')))
+    model.to(device)
     # model = torch.load(disk_path, map_location=torch.device('cpu')).to(device)
     return model, device
 
